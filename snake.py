a3daf12c0fd8cd6594bd37041d5d05b608efece8
NANOUU <59022292+antoniasymeonidou@users.noreply.github.com>
2021-04-25 11:27:03 +0100

    Create snake.py

---
+++ snake.py
@@ -0,0 +1,48 @@
+import pygame, sys, time, random
+
+difficulty = 25
+
+w = 700
+h = 500
+
+pygame.init()
+
+# Initialise game window
+pygame.display.set_caption('Snake Game')
+game_window = pygame.display.set_mode((w, h))
+
+# Colors (R, G, B)
+black = pygame.Color(0, 0, 0)
+white = pygame.Color(255, 255, 255)
+red = pygame.Color(255, 0, 0)
+green = pygame.Color(0, 255, 0)
+blue = pygame.Color(0, 0, 255)
+
+# FPS (frames per second) controller
+fps = pygame.time.Clock()
+
+# Game variables
+position = [100, 50]
+snake_body = [[100, 50], [100-10, 50], [100-(2*10), 50]]
+
+food_pos = [random.randrange(1, (w//10)) * 10, random.randrange(1, (h//10)) * 10]
+food_spawn = True
+
+direction = 'RIGHT'
+change_to = direction
+
+score = 0
+
+# Game Over
+def game_over():
+    my_font = pygame.font.SysFont('times new roman', 90)
+    game_over_surface = my_font.render('YOU DIED', True, red)
+    game_over_rect = game_over_surface.get_rect()
+    game_over_rect.midtop = (w/2, h/4)
+    game_window.fill(black)
+    game_window.blit(game_over_surface, game_over_rect)
+    show_score(0, red, 'times', 20)
+    pygame.display.flip()
+    time.sleep(3)
+    pygame.quit()
+    sys.exit()
