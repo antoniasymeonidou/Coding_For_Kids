6a03ce9fd140d3bc5f7cbf36cc557e298010c3cf
NANOUU <59022292+antoniasymeonidou@users.noreply.github.com>
2021-05-03 12:03:26 +0100

    Create wizard-shopping-list.py

---
+++ wizard-shopping-list.py
@@ -0,0 +1,12 @@
+list = ['frog legs', 'bat eye', 'butterfly wing', 'pig ear']
+print(list)
+list[2] = 'magic sprinkles'
+print(list)
+list.append('scary tail')
+print(list)
+list.append('tongue')
+print(list)
+amount = len(list)
+print(amount)
+del list[0]
+print(list)
