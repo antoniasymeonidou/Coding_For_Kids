cbc373b97897de4c9d999bbb8e7361c12d3590e0
NANOUU <59022292+antoniasymeonidou@users.noreply.github.com>
2021-05-03 11:50:10 +0100

    Create poem.py

---
+++ Python/poem.py
@@ -0,0 +1,7 @@
+color = input("Enter your favourite color: ")
+pluralNoun = input("Enter a plural noun: ")
+game = input("Enter your favourite game: ")
+
+print("Roses are", color)
+print(pluralNoun + " are blue")
+print("I love", game)
